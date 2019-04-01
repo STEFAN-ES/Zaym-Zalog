28effdd36a05e83d48e5179f38a1693bf0a98456
STEFAN-ES <32038779+STEFAN-ES@users.noreply.github.com>
2019-04-01 21:06:21 +0300

    пытаемся сделать слайдер на owl

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
   $('.partners-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
     centermode: true,
     infinite: true,
-    prevArrow: '<div class="partners-slider__arrow-left"></div>',
-    nextArrow: '<div class="partners-slider__arrow-right"></div>',
+    arrows: true,
+    prevArrow: '<div class="partners-slider__arrow partners-slider__arrow-left"></div>',
+    nextArrow: '<div class="partners-slider__arrow partners-slider__arrow-right"></div>'
   });
+}); */
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
 });
