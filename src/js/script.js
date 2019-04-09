d66a1e4742c90859cd6fa1c7fc744a4565552157
STEFAN-ES <32038779+STEFAN-ES@users.noreply.github.com>
2019-04-09 11:55:42 +0300

    полный PP десктопный сайт, без адаптива

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,145 @@ function modalclose(){
 
 closemodal.on('click', modalclose);
 callModal.on('click', modal);
+
+
+
+$( "#number-slider" ).slider({
+    animate: "slow",
+    range: "min",    
+    value: 0.3,
+    min: 0.3,
+    step: 0.1,
+    max: 30.1,
+    slide : function(event, ui) {
+    	  var valueSl = ui.value;
+    	  if(ui.value > 30){
+    	  	valueSl = 30;
+    	  }
+          $("#send-result-polzunok").val(valueSl);
+          $(".sumMoney").text(valueSl +" млн. руб.");
+          activMoney = valueSl;
+          summFinal ( activMoney , activYears);
+      }
+});
+$("#send-result-polzunok").val($("#number-slider").slider("value"));
+$(".sumMoney").text($("#number-slider").slider("value")+" млн. руб.");
+
+
+$( "#year-slider" ).slider({
+    animate: "slow",
+    range: "min",    
+    value: 1,
+    min: 1,
+    step: 1,
+    max: 20,
+    slide : function(event, ui) {
+    		 var textYear = ' год.';
+    		 if(ui.value === 1){
+    		 	textYear = ' год.';
+    		 }else if(ui.value > 1 && ui.value < 5){
+    		 	textYear = ' года.';
+    		 }else {
+    		 	textYear = ' лет.';
+    		 }
+          $("#send-result-year").val(ui.value);
+          $(".sumYear").text(ui.value + textYear);
+          activYears = ui.value;
+          summFinal ( activMoney , activYears);
+      }
+});
+$("#send-result-year").val($("#year-slider").slider("value"));
+$(".sumYear").text($("#year-slider").slider("value")+" год.");
+
+var _S = $("#number-slider").slider("value");
+var _P = 0.02;
+var _n = $("#year-slider").slider("value");
+var formula = _S*( _P + (_P/ (Math.pow((1 + _P), _n*12) - 1)));
+var formula = Math.floor(formula * 1000000);
+var outputn = String(formula).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ');
+
+$('.calc-output__num').text(outputn);
+
+var activMoney = $("#number-slider").slider("value") ;
+var activYears = $("#year-slider").slider("value");
+function summFinal ( money , years){
+	_P = 0.02;
+	var formulaFinal = money *( _P + (_P/ ((Math.pow((1 + _P), years*12)) - 1)));
+  var formulaFinal = Math.floor(formulaFinal * 1000000);
+  var output = String(formulaFinal).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ');
+  $('.calc-output__num').text(output);
+}
+
+
+
+
+
+$('.partners-slider').slick({
+  speed: 400,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+
+$('form').submit(function(event) {
+
+      event.preventDefault();
+
+      var action = "mailer/smart.php";
+      var msg = $(this).serialize();
+      var formThis = $(this);
+
+      $.ajax({
+          type: "POST",
+          url: action,
+          data: msg,
+          success: function(data) {
+
+
+              if(formThis.find('input[type="hidden"]').val() === "main-form" ){
+
+                $(".overlay").fadeOut();
+                $('html').addClass('stop');
+                $("#download-start").fadeIn();
+
+              }else if(formThis.find('input[type="hidden"]').val() === "gagerModal"){
+                $(".overlay").fadeOut();
+                $('html').addClass('stop');
+                $("#modal-thanks").fadeIn();
+              }
+
+              $('form').trigger('reset');
+
+
+          },
+          error: function(xhr, str) {
+
+              alert('Произошла ошибка, попробуйте немного позже');
+          }
+      });
+  });
